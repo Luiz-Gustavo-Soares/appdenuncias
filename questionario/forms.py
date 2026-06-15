f91ac3a18943116f83d1b815cd4a4b18ebbf462d
italoRavix <iperfilteste@gmail.com>
2026-06-15 15:16:51 -0300

    Testando o forms com html

--- questionario/forms.py
+++ questionario/forms.py
@@ -2,9 +2,15 @@ from django import forms
 
 from questionario.models import Questionario
 
-
+# FORM TESTE SO PARA VER SE FUNCIONOU PODE APAGAR CALABRESO
 class QuestionarioForm(forms.ModelForm):
 
     class Meta:
         model = Questionario
         fields = "__all__"
+
+        widgets = {
+            "data_fato": forms.DateInput(attrs={"type": "date"}),
+            "data_nascimento_vitima": forms.DateInput(attrs={"type": "date"}),
+            "hora_aproximada_fato": forms.TimeInput(attrs={"type": "time"}),
+        }
